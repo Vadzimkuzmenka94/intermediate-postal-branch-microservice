253535fdcb5063e4c6beb36d5d8cdaac49393363
Vadzim Kuzmenka <vadzimkuzmenka@mail.ru>
2023-09-08 13:15:42 +0400

    fix

--- src/main/java/ru/skyeng/test/project/config/SchedulingConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.skyeng.test.project.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulingConfig {
-}
--- src/main/java/ru/skyeng/test/project/kafka/Consumer.java
+++ src/main/java/ru/skyeng/test/project/kafka/Consumer.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.skyeng.test.project.kafka.event.ParcelDeliveryEvent;
 
